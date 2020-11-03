c7cb203e7b1110da54930b9beaf9ea5a1ae42a40
Navneet Yadav <67457776+sagnav@users.noreply.github.com>
2020-11-03 17:30:44 +0530

    Update opencv_video_basics.py
    adding video record and save function

--- opencv_video_basics.py
+++ opencv_video_basics.py
@@ -7,5 +7,29 @@ Created on Tue Nov  3 14:45:42 2020
 # %% 
 import numpy as np
 import cv2
-# %%
- 
+# %% recording, saving video
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+
+        # write the flipped frame
+        out.write(frame)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
